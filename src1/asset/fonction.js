464da54cc6ff2e01df379ebd16c2eb6546962910
MeldryckSAID <meldryck.said@edu.univ-fcomte.fr>
2022-01-07 16:54:32 +0100

    carrousel infi

--- src1/asset/fonction.js
+++ src1/asset/fonction.js
@@ -36,29 +36,7 @@ function carrouselBasic() {
     });
 }
 
-//  function monterLegende(c) 
-//      let texte13 = document.querySelector("."+c);
-//      texte13.style.display = "block";
-//      return anime({
-//          targets: "."+c,
-//          translateY: '-5em',
-//          autoplay: false,
-//          easing: 'linear',      
-//          duration: 200
-//      }).pla
- 
-//  function descendreLegende(c) {
-//      let txt1 = document.querySelector("."+c);
-//      txt1.style.display = "none";
-//      return anime({
-//          targets: "."+c,
-//          translateY: '0',
-//          autoplay: false,
-//          easing: 'linear',      
-//          duration: 200
-//      }).play
 
-//  }
 // 1
  function monterLegende() {
      let texte13 = document.querySelector(".texte__js1");
@@ -181,4 +159,48 @@ function descendreLegende5() {
         easing: 'linear',
         duration: 200
     }).play;
+}
+
+function carrousel6img() {
+    var carr6 = document.querySelectorAll('.carou6');
+    let d = 2000;
+    let delta = 1000;
+
+    carr6.forEach(function(img,indice) {
+        img.style.zIndex = carr6.length - indice;
+    });
+
+    let anim4 = anime.timeline({
+        loop: true,
+        delay: 0
+    });
+
+    anim4.add({
+        targets: carr6,
+        translateX: [{value: '375', duration: d},
+                     {value: '-375', duration: 0, delay: function(img,ind) {
+                         if (ind == 0) return (5*delta)+(4*d);
+                         if (ind == 1) return (4*delta)+(3*d);
+                         if (ind == 2) return (3*delta)+(2*d);
+                         if (ind == 3) return (2*delta)+d;
+                         if (ind == 4) return delta;
+                         return 0;
+                     }
+                    },
+                     {value: '0', duration: function(img,ind) {
+                         if (ind == 5) return 0;
+                         return d;
+                     }
+                }],
+        easing: 'linear',
+        delay: function(img,ind) {
+            if (ind == 0) return delta;
+            if (ind == 1) return (2*delta)+d;
+            if (ind == 2) return (3*delta)+(2*d);
+            if (ind == 3) return (4*delta)+(3*d);
+            if (ind == 4) return (5*delta)+(4*d);
+            return (6*delta)+(5*d);
+        }
+    });
+
 }
